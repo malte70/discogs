9ffcdf8cf4b797c6b11905db701a0fa35672d656
Malte Bublitz <malte@rolltreppe3.de>
2025-07-25 21:18:04 +0200

    Configurable icon for random_vinyl.php

--- config.inc-dist.php
+++ config.inc-dist.php
@@ -10,4 +10,6 @@ define("COLLECTION_ITEMS_CACHE_FILE", CACHE_DIR . "/" . "collection_items.cache"
 define("CACHE_TIME", "-1 hours");
 define("USER_AGENT", "malte70-discogs/0.1 +https://github.com/malte70/discogs");
 define("DISCOGS_FORMAT_FILTER", "Vinyl");
+//define("FAVICON_URL", "https://xyz.malte70.de/img/icons_tango/audio-x-generic-512.png");
+define("FAVICON_URL", "https://xyz.malte70.de/img/icons_tango/itunes-512.png");
 
--- random_vinyl.php
+++ random_vinyl.php
@@ -117,7 +117,7 @@ if (!file_exists($img_local_filename)) {
 
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 		<link rel="stylesheet" href="style.css">
-		<link rel="icon" type="image/png" href="https://dev.malte70.de/tango-icons/img/itunes-512.png">
+		<link rel="icon" type="image/png" href="<?=FAVICON_URL?>">
 	</head>
 	<body id="top">
 		<main>
